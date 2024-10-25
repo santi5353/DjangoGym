2705f71866b7c5192696670c742bff88a367df3a
santiago solano <santiagosolano036@gmail.com>
2024-10-25 10:10:35 -0300

    estilos carpeta static

--- DjangoGym/djangocrud/urls.py
+++ DjangoGym/djangocrud/urls.py
@@ -1,21 +1,8 @@
-"""djangocrud URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from tasks import views
+from django.conf import settings  # Asegúrate de importar settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -27,6 +14,6 @@ urlpatterns = [
     path('signin/', views.signin, name='signin'),
     path('create_task/', views.create_task, name='create_task'),
     path('tasks/<int:task_id>', views.task_detail, name='task_detail'),
-    path('taks/<int:task_id>/complete', views.complete_task, name='complete_task'),
+    path('tasks/<int:task_id>/complete', views.complete_task, name='complete_task'),
     path('tasks/<int:task_id>/delete', views.delete_task, name='delete_task'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
